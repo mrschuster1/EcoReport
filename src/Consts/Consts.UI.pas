66387ab9ddf3ce593f3948f1ef5bd5e7e8194bec
Thiago Schuster <99575284+mrschuster1@users.noreply.github.com>
2023-12-16 11:18:34 -0300

    Unit with UI consts
    Icons, colors, etc

---
+++ src/Consts/Consts.UI.pas
@@ -0,0 +1,18 @@
+﻿unit Consts.UI;
+
+interface
+
+{$REGION 'Control icons' default=folded}
+/// Control icons
+
+const
+  UI_MAXIMIZE = '';
+  UI_RESTORE = '';
+  UI_DARK = '';
+  UI_LIGHT = '';
+
+{$ENDREGION}
+
+implementation
+
+end.
